ca69b00969bcd1dcc251d4e8848b931104776511
LuisinhoDev <luiseduardodcf@gmail.com>
2023-10-02 09:04:00 -0300

    Add project files.

---
+++ TarefaPWTeste2/Controllers/FilmeController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TarefaPWTeste2.Models;
+
+namespace TarefaPWTeste2.Controllers
+{
+    public class FilmeController : Controller
+    {
+        // GET: FilmeController
+        public ActionResult Index()
+        {
+            return View(FilmeModel.CriarLista());
+        }
+
+        // GET: FilmeController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(FilmeModel.CriarLista()[id]);
+        }
+
+        // GET: FilmeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FilmeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FilmeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(FilmeModel.CriarLista()[id]);
+        }
+
+        // POST: FilmeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FilmeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(FilmeModel.CriarLista()[id]);
+        }
+
+        // POST: FilmeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ TarefaPWTeste2/Models/FilmeModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TarefaPWTeste2.Models
+{
+    public class FilmeModel
+    {
+        public String Nome { get; set; }
+        public string Genero { get; set; }
+        public DateTime Lançamento { get; set; }
+        public int Nota { get; set; }
+
+        public FilmeModel()
+        {
+            Nome = "Rango";
+            Genero = "Animação/Comédia";
+            Lançamento = new DateTime(2011, 3, 9);
+            Nota = 10;
+        }
+        public static FilmeModel CriarFilme()
+        {
+            var filme = new FilmeModel();
+            filme.Nome = "Jojo Rabbit";
+            filme.Genero = "Comédia/Drama";
+            filme.Lançamento = new DateTime(2018, 2, 6);
+            filme.Nota = 8;
+
+            return filme;
+
+        }
+
+        public static List<FilmeModel> CriarLista()
+        {
+            var lista = new List<FilmeModel>();
+            lista.Add(new FilmeModel());
+            lista.Add(FilmeModel.CriarFilme());
+
+            return lista;
+        }
+    }
+}
